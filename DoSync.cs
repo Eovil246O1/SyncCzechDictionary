8fdf60f1623d92571323a484eebc51354a9e1485
Eovil246O1 <viacheslav.pyrohov@gmail.com>
2022-07-30 11:47:07 +0200

    Add project

---
+++ DoSync.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using static System.Console;
+
+string targetPath = @"A:\Sync\Selfeducation\Languages\TutorDict\Czech LearnCsCs.xml";
+string originalPath = @"C:\Users\Eovil\AppData\Local\ABBYY\Lingvo\15.0\Dic\TutorDict\Czech LearnCsCs.xml";
+
+Console.WriteLine("Staring to copy file...");
+
+File.Copy(originalPath, targetPath, overwrite: true);
+
+Console.WriteLine($"File successfully copied for {DateTime.Now.ToString("dd/MM/yyyy")}");
